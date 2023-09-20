b7c85b4233e99e9a456a45b01aaa631d1071506a
RexshackGaming <rexshackuk@gmail.com>
2023-09-20 06:44:41 +0100

    prompts and blips if needed

--- client/client.lua
+++ client/client.lua
@@ -28,6 +28,25 @@ local function comma_value(amount)
 end
 
 -------------------------------------------------------------------------------------------
+-- prompts and blips if needed
+-------------------------------------------------------------------------------------------
+Citizen.CreateThread(function()
+    for _, v in pairs(Config.GangLocations) do
+        exports['rsg-core']:createPrompt(v.id, v.coords, RSGCore.Shared.Keybinds[Config.Keybind], 'Open '..v.name, {
+            type = 'client',
+            event = 'rsg-gangmenu:client:mainmenu',
+            args = {},
+        })
+        if v.showblip == true then
+            local GangMenuBlip = Citizen.InvokeNative(0x554D9D53F696D002, 1664425300, v.coords)
+            SetBlipSprite(GangMenuBlip,  joaat(Config.Blip.blipSprite), true)
+            SetBlipScale(Config.Blip.blipScale, 0.2)
+            Citizen.InvokeNative(0x9CB1A1623062F402, GangMenuBlip, Config.Blip.blipName)
+        end
+    end
+end)
+
+-------------------------------------------------------------------------------------------
 -- main menu
 -------------------------------------------------------------------------------------------
 RegisterNetEvent('rsg-gangmenu:client:mainmenu', function()
--- config.lua
+++ config.lua
@@ -1,1 +1,22 @@
 Config = {}
+
+-- blip settings
+Config.Blip = {
+    blipName = 'Gang Menu', -- Config.Blip.blipName
+    blipSprite = 'blip_honor_bad', -- Config.Blip.blipSprite
+    blipScale = 0.2 -- Config.Blip.blipScale
+}
+
+-- settings
+Config.Keybind = 'J'
+
+Config.GangLocations = {
+
+    {   -- example
+        id = 'gang1',
+        name = 'Gang Menu',
+        coords = vector3(-274.88, 805.72, 119.37),
+        showblip = true
+    },
+
+}
